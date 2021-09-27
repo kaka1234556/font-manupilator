9c5ac04155d5354202a0cc89d4a89ab3a369c708
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-09-27 14:20:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
   function setup() {
   video = createCapture(VIDEO);
@@ -21,11 +23,15 @@ function gotPoses(results)
   if(results.length > 0)
   {
     console.log(results);
-}
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
 function draw() {
 background('#6C91C2');
-
 }
-
